8c21d5ea24422c777f648a42554e9f10c539fd77
ryan_ubuntu <ryandx0de@outlook.com>
2022-07-13 13:06:05 +0800

    ...

---
+++ face_recognition_cameracapture/camera_capture_face.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+capture = cv.VideoCapture(2)
+capture.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+capture.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+cv.namedWindow("frame", cv.WINDOW_AUTOSIZE)
+
+while True:
+    ret, frame = capture.read()
+    if ret:
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        faceCascade = cv.CascadeClassifier('face_detect.xml')
+        faceRect = faceCascade.detectMultiScale(gray, 1.1, 8)
+        for (x, y, w, h) in faceRect:
+            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv.imshow('frame', frame)
+    else:
+        break
+    # if cv.waitKey(10) == ord('q'):
+    if cv.waitKey(10) == 27:
+        break
