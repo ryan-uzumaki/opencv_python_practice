389203499a66c9256d338bad095638c7d9d36d84
Ryan Uzumaki <ryandx0de@outlook.com>
2022-07-17 07:15:11 +0800

    ...

--- channels_split.py
+++ channels_split.py
@@ -2,8 +2,8 @@ import cv2 as cv
 
 image = cv.imread("test.png")
 
-# img = cv.resize(image, dsize=None, fx=0.2, fy=0.2)
-img = cv.resize(image, (image.shape[1]//2, image.shape[0]//2))
+img = cv.resize(image, dsize=None, fx=0.2, fy=0.2)
+# img = cv.resize(image, (image.shape[1]//2, image.shape[0]//2))
 cv.imshow("img", img)
 # blur = cv.GaussianBlur(img, (5, 5), cv.BORDER_DEFAULT)
 # cv.imshow('Blur', blur)
@@ -13,20 +13,23 @@ cv.imshow("split channelB", B)
 cv.imshow("split channelG", G)
 cv.imshow("split channelR", R)
 
-bmg = B - G
-gmb = G - B
-cv.imshow("bmg", bmg)
-cv.imshow("gmb", gmb)
-rmb = R - B
-bmr = B - R
-cv.imshow("rmb", rmb)
-cv.imshow("bmr", bmr)
-rmg = R - G
-gmr = G - R
-cv.imshow("rmg", rmg)
-cv.imshow("gmr", gmr)
-ksize = (5, 5)
-image_blur = cv.blur(bmg, ksize)
+
+bmg = B-G
+cv.imshow("bmgmr", bmg)
+# bmg = B - G
+# gmb = G - B
+# cv.imshow("bmg", bmg)
+# cv.imshow("gmb", gmb)
+# rmb = R - B
+# bmr = B - R
+# cv.imshow("rmb", rmb)
+# cv.imshow("bmr", bmr)
+# rmg = R - G
+# gmr = G - R
+# cv.imshow("rmg", rmg)
+# cv.imshow("gmr", gmr)
+# ksize = (5, 5)
+# image_blur = cv.blur(bmg, ksize)
 # cv.imshow("image_blur", image_blur)
 # image_result = cv.threshold(image_blur, 50, 255, cv.THRESH_BINARY)
 # # gmr = channel[1]-channel[2]
@@ -35,3 +38,6 @@ image_blur = cv.blur(bmg, ksize)
 # # cv.imshow("rmg", rmg)
 # cv.imshow("image_result", image_result)
 cv.waitKey(0)
+
+
+# cv.calcHist(img, [0], None, [255], [0,255])
--- face_recognition_cameracapture/camera_capture_face.py
+++ face_recognition_cameracapture/camera_capture_face.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-capture = cv.VideoCapture(2)
+capture = cv.VideoCapture(1)
 capture.set(cv.CAP_PROP_FRAME_WIDTH, 640)
 capture.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
 cv.namedWindow("frame", cv.WINDOW_AUTOSIZE)
@@ -10,7 +10,7 @@ while True:
     if ret:
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         faceCascade = cv.CascadeClassifier('face_detect.xml')
-        faceRect = faceCascade.detectMultiScale(gray, 1.1, 8)
+        faceRect = faceCascade.detectMultiScale(gray, 1.1, 6)
         for (x, y, w, h) in faceRect:
             cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv.imshow('frame', frame)
